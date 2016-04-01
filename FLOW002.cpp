f73a6bc18806aacaab9283d29fbecb127ef3f6cd
crazysj <crazysj@users.noreply.github.com>
2016-04-01 23:49:31 +0530

    Added files via upload

---
+++ FLOW002.cpp
@@ -0,0 +1,23 @@
+//https://www.codechef.com/problems/FLOW002
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        ll n,k,temp1,temp2;
+        cin >> n >> k;
+
+          temp2= n%k;
+          cout << temp2  << endl;
+
+
+
+    }
+
+    return 0;
+}
